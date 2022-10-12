b55d648886c03505dac3d292ebf63197044afcb3
Paul Katumba <paulkatumba@minute5.ug>
2022-10-12 05:26:19 +0000

    simple code to exit when none numerical data is typed

--- intro.py
+++ intro.py
@@ -1,6 +1,12 @@
 #print("Hello world! My name is Papatumba fron Uganda")
 #simple python program to add two number and print the outcome
-a=int(input("Enter the first number: "))
-b=int(input("Enter the second number: "))
+a=0
+b=0
+try:
+    a=int(input("Enter the first number: "))
+    b=int(input("Enter the second number: "))
+except:
+    print("Please enter a number")
+    quit()
 sum=a+b
-print(sum)
+print(f"The sum of {a} and {b} is {sum}")
