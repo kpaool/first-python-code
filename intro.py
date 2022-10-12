a66de7538676ac5c17719bbc1f95f5ab4ded19b0
Paul Katumba <paulkatumba@minute5.ug>
2022-10-12 05:15:38 +0000

    main branch made

--- intro.py
+++ intro.py
@@ -1,6 +1,6 @@
-print("Hello world! My name is Papatumba fron Uganda")
+#print("Hello world! My name is Papatumba fron Uganda")
 #simple python program to add two number and print the outcome
-a=int(input("Enter the first number"))
-b=int(input("Enter the second number"))
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
 sum=a+b
 print(sum)
