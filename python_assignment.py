f2ea7459ac0451077cb685285b40932c036f21fc
Katumba <jonathanpkatumba@gmail.com>
2022-10-18 19:08:20 +0000

    basic implementation of the ahmed problem

--- python_assignment.py
+++ python_assignment.py
@@ -1,4 +1,4 @@
-#pre
+#preamble
 def even_numbers_for_loop():
     for number in range(1,31):
         if number%2==0:
@@ -21,4 +21,12 @@ def odd_numbers_while_loop():
         print(odd_number)
         odd_number+=2
 
-#
+#ahmed budgetting issue
+
+total_budget=50000.00
+marketing= 0.25*total_budget
+operations= 0.5*total_budget
+customer_acquisition_cost= 0.25*total_budget
+cac_per_customer=5.00
+acquired_customers= int(customer_acquisition_cost/cac_per_customer)
+print(acquired_customers)
