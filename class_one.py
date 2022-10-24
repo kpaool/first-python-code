1eb62bb2797021aeb102a7b9d2052ceb887dbac3
Katumba <jonathanpkatumba@gmail.com>
2022-10-24 09:45:45 +0000

    first commit for class

---
+++ class_one.py
@@ -0,0 +1,18 @@
+class Car():
+    color=""
+    model=""
+    speed=0
+    brand=""
+    fuel_tank=0
+    mileage=0
+    engine_size=0
+    number_of_whels=0
+
+car1 = Car()
+
+car1.color="Blue"
+car1.speed=200
+car1.model="Model S"
+car1.brand="Tesla"
+
+print(f"Mary's car model: {car1.model}")
