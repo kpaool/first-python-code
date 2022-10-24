8729bf1fb84bc4b469a52ed5ee542b36ea5d9f7b
Katumba <jonathanpkatumba@gmail.com>
2022-10-24 10:54:20 +0000

    Using python classes

--- class_one.py
+++ class_one.py
@@ -8,11 +8,19 @@ class Car():
     engine_size=0
     number_of_whels=0
 
+    def __init__(self,brand,model) -> None:
+        self.brand=brand
+        self.model=model
+
+    def navigation(self,direction):
+        print(f"Turn {direction}")
+
 car1 = Car()
 
 car1.color="Blue"
 car1.speed=200
 car1.model="Model S"
 car1.brand="Tesla"
+car1.navigation("Left")
 
 print(f"Mary's car model: {car1.model}")
