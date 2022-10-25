b4301f6636e35320132fdfd93e188a2fedbb4ee7
Katumba <jonathanpkatumba@gmail.com>
2022-10-25 13:32:33 +0000

    tech class 25/10/22

--- class_one.py
+++ class_one.py
@@ -15,12 +15,12 @@ class Car():
     def navigation(self,direction):
         print(f"Turn {direction}")
 
-car1 = Car()
+
+
+car1 = Car(model="Toyota",brand="2007 Camry")
 
 car1.color="Blue"
 car1.speed=200
-car1.model="Model S"
-car1.brand="Tesla"
 car1.navigation("Left")
 
 print(f"Mary's car model: {car1.model}")
